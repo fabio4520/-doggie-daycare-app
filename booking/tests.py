23740eb8b0a716f5b25ce5707089d0bf182b3cb7
Fabio Fiestas <fabioleofc@gmail.com>
2024-03-07 23:01:35 -0500

    ADD: tests for CustomerTest

--- booking/tests.py
+++ booking/tests.py
@@ -8,29 +8,44 @@ from .models import *
 class CustomerTest(TestCase):
     
     def setUp(self):
-       # Code Part 3.1 here
-       pass 
+        self.customer = Customer.objects.create(
+            first_name="Fabio",
+            last_name="Fiestas",
+            email="fabioleofc@gmail.com"
+        )
 
     def tearDown(self):
-        # Code Part 3.1 here
-        pass
+        self.customer.delete()
 
     def test_create(self):
-        # Code Part 3.1 here
-        pass
+        customer = Customer.objects.create(
+            first_name="Fabio",
+            last_name="Fiestas",
+            email="fabioleofc@gmail.com"
+        )
+        self.assertIsInstance(customer, Customer)
 
     def test_read(self):
-        # Code Part 3.1 here
-        pass
-        
+        self.assertEqual(self.customer.first_name, "Fabio")
+        self.assertEqual(self.customer.last_name, "Fiestas")
+        self.assertEqual(self.customer.email, "fabioleofc@gmail.com")
+
     def test_update(self):
-        # Code Part 3.1 here
-        pass
+        self.customer.first_name = "UpdatedName"
+        self.customer.last_name = "UpdatedLastName"
+        self.customer.email = "updatedfabioleofc@gmail.com"
+        self.customer.save()
+
+        updated_customer = Customer.objects.get(id=self.customer.id)
+        self.assertEqual(updated_customer.first_name, "UpdatedName")
+        self.assertEqual(updated_customer.last_name, "UpdatedLastName")
+        self.assertEqual(updated_customer.email, "updatedfabioleofc@gmail.com")
 
     def test_delete(self):
-        # Code Part 3.1 here
-        pass
-
+        customer_id = self.customer.id
+        Customer.objects.filter(id = customer_id).delete()
+        with self.assertRaises(Customer.DoesNotExist):
+            Customer.objects.get(id=customer_id)
 
 ######################  Dog Model Tests ######################
 
